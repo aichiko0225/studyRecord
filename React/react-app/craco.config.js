8699473f725dd92afc37aca0238053aca894fe6c
ash66 <aichiko66@163.com>
2025-01-14 17:13:16 +0800

    add react app

---
+++ React/react-app/craco.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      return {
+        ...webpackConfig,
+        output: {
+          ...webpackConfig.output,
+          // filename: 'static/js/[name].[contenthash:8].js',
+        }
+      }
+    },
+  }
+}
